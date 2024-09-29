5cd075148efe34f6e4843fddcd459ad4c867fa87
br25 <md.ashrafulislamrobin12345@gmail.com>
2024-09-29 19:21:06 +0600

    initial template

--- todo_app/views.py
+++ todo_app/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import loader
 
 
 def todo_app(request):
-    return HttpResponse("Hel")
+    template = loader.get_template("main.html")
+    return HttpResponse(template.render())
